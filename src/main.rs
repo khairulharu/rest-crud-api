a435ef821923518e3f2259a29539e17c55e85c98
khairulharu <khairulaswad719@gmail.com>
2025-01-14 16:41:33 +0800

    New: Handle Deleted User

--- src/main.rs
+++ src/main.rs
@@ -153,6 +153,24 @@ fn handle_put_request(request: &str) -> (String, String) {
     }
 }
 
+//handle delete request
+fn handle_delete_request(request: &str) -> (String, String) {
+    match (get_id(&request).parse::<i32>(), Client::connect(DB_URL, NoTls)) {
+        (Ok(id), Ok(mut client)) => {
+            let rows_affected = client.execute(
+                "DELETE FROM users WHERE id = $1",
+                &[&id])
+                .unwrap();
+
+            if rows_affected == 0 {
+                return (NOT_FOUND.to_string(), "Users not found".to_string());
+            }
+
+            (OK_RESPONSE.to_string(), "User deleted".to_string())
+        },
+        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string())
+    }
+}
 
 //database connection
 fn set_database() -> Result<(), PostgresError> {
